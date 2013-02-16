c32d39f16772fadeb52930a6e1b6d590452694c1
Alex Nichol <alexnichol@comcast.net>
2013-02-15 21:45:21 -0500

    Initial Commit

---
+++ Electrostatics/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  Electrostatics
+//
+//  Created by Alex Nichol on 2/15/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
